a97048fb9251b25f04a5b1e96229c00a8586ec47
James Nash <jmnxvii@gmail.com>
2025-12-24 11:25:50 +0000

    Securtiy update

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from '@vercel/edge'
+
+export const config = {
+  matcher: '/:path*',
+}
+
+export default function middleware(request) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (user === 'admin' && pwd === process.env.STORYBOOK_PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Storybook"',
+    },
+  })
+}
